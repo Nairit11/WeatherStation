0bb626c045026cd97660996bee90a039ff82e3c4
nairit <nairit32@gmail.com>
2017-03-26 13:56:52 +0530

    Final website

--- mainWeather.js
+++ mainWeather.js
@@ -17,7 +17,7 @@ var mainWeather = {
             success: function (data) {
                 var wrapper = $("#WeatherWrapper");
                 wrapper.empty();
-                wrapper.append("<div class='city'> <p>Place: " + data.name + ", " + data.sys.country + "</p></div>");
+                wrapper.append("<div class='city'> <p><b>Place: </b>" + data.name + ", " + data.sys.country + "</p></div>");
                 wrapper.append(mainWeather.createWeatherWidg(data));
             },
             error: function () {
@@ -30,12 +30,12 @@ var mainWeather = {
       
        createWeatherWidg: function (data) {
 //console.log("hi");
-        return "<div class='pressure'> <p>Temperature: " + (data.main.temp - 273.15).toFixed(2) + " C</p></div>"+
-                "<div class='description'> <p>Title: " + data.weather[0].main + "</p></div>" +
-                "<div class='description'> <p>Description: " + data.weather[0].description + "</p></div>" +
-                "<div class='wind'> <p>Wind Speed: " + data.wind.speed + "</p></div>" +
-                "<div class='humidity'> <p>Humidity: " + data.main.humidity + "%</p></div>" +
-                "<div class='pressure'> <p>Pressure: " + data.main.pressure + " hpa</p></div>";
+        return "<div class='pressure'> <p><b>Temperature:</b> " + (data.main.temp - 273.15).toFixed(2) + " C</p></div>"+
+                "<div class='description'> <p><b>Title:</b> " + data.weather[0].main + "</p></div>" +
+                "<div class='description'> <p><b>Description:</b> " + data.weather[0].description + "</p></div>" +
+                "<div class='wind'> <p><b>Wind Speed: </b>" + data.wind.speed + "</p></div>" +
+                "<div class='humidity'> <p><b>Humidity:</b> " + data.main.humidity + "%</p></div>" +
+                "<div class='pressure'> <p><b>Pressure: </b>" + data.main.pressure + " hpa</p></div>";
 		"alert('hi');"
     }
 }
